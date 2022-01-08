f6df02615ed45f07385680708efd4cd1f7580ce9
Andrew Hansraj <hansrajandrew@gmail.com>
2022-01-08 16:49:54 -0500

    Added files from local machine

---
+++ magicFormulaInvesting.py
@@ -0,0 +1,38 @@
+# Based off of Joel Greenblatt's magic formula investing strategy
+# In his book "The Little Book That Beats the Market"
+# Though the strategy is a bit old now since it was created in 2005
+# His newer book (2010) has an updated strategy
+# The book is called "The Little Book That Still Beats the Market"
+import yfinance as yf
+
+# Should not be a financial or utility company
+# Also exclude foreign companies (e.g. CDR or ADR)
+symbol = 'MSFT'
+
+MSFT = yf.Ticker(symbol)
+
+marketCap = MSFT.info['marketCap']
+minimum_marketCap = 100000000
+
+if marketCap < minimum_marketCap:
+    print("Market capitalization needs to be greater than 100 million")
+
+# Easy way of getting EBITDA and EV/EBITDA
+
+yf_EBITDA = MSFT.info['ebitda']
+yf_EVtoEBITDA = MSFT.info['enterpriseToEbitda']
+
+# Harder way of getting EBITDA. Need to look at financial statements
+# Data found for 6/30/2021
+
+netIncome = 61271000000  # income statement
+normalizedIncome = 60150420000
+taxExpense = 9831000000  # income statement
+interestExpense = 2346000000  # income statement
+depreciationAmortization = 11686000000  # cash flow statement
+
+EBITDA = netIncome + taxExpense + interestExpense + depreciationAmortization
+
+# Harder way of getting EV/EBITDA
+
+enterprise_value = 0
